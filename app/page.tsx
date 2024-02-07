be7ba93d836d0c2ad7061874aba5736cb5a19df5
Luke Cassady-Dorion <luke@SPStories.com>
2024-02-07 13:23:02 -0300

    first push

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import type { Metadata } from "next";
+
+const frameMetadata = getFrameMetadata({
+	buttons: [
+		{
+			label: "Begin",
+		},
+	],
+	image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/llama-graffiti.png`,
+	post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/graffiti?word=yoyoyo`,
+});
+
+export const metadata: Metadata = {
+	title: "Llama graffiti artist",
+	description: "Llama paints the night, Eternal data in art, Walls whisper always.",
+	openGraph: {
+		title: "Llamas make the best artists",
+		description: "Llama paints the night, Eternal data in art, Walls whisper always.",
+		// images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`],
+	},
+	other: {
+		...frameMetadata,
+	},
+};
+
+export default function Page() {
+	return (
+		<>
+			<h1>Llama paints the night, Eternal data in art, Walls whisper always.</h1>
+		</>
+	);
+}
